fdbd3610e68e53c4f32a02a7438b558fd46521f1
demetrebiga <demebiga@gmail.com>
2024-03-20 17:01:14 +0400

    first commit

--- day07/lesson 8.2 .py
+++
@@ -1,4 +0,0 @@
-first_number = int(input ("10:   ") )
-secand_number = int(input ("20:  ") )
-answer = first_number / secand_number
-print(answer)
---
+++ day07/lesson07.2 .py
@@ -0,0 +1,4 @@
+first_number = int(("10:   ") )
+secand_number = int(("20:  ") )
+answer = first_number / secand_number
+print(answer)
