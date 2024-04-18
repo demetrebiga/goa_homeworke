f751af35a8b6593cf0bf4cf59e90d4c90776a528
demetrebiga <demebiga@gmail.com>
2024-04-18 10:28:29 +0400

    second commit

---
+++ day22/claswork.py
@@ -0,0 +1,17 @@
+
+
+best_academy = input("what is the best academy?:  ")
+
+
+while best_academy != "GOA":
+    best_academy = input("what is the best academy?")
+
+
+print('GOA BESTTT')
+
+
+
+
+
+
+
