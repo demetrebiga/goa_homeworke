1765b9a7b2d5af4adbcfcb3692fa4a137050e7be
demetrebiga <demebiga@gmail.com>
2024-04-30 19:37:31 +0400

    second commit

---
+++ day26/lesson26.py
@@ -0,0 +1,39 @@
+GOA = ("yes")
+
+hmm = input ("do you learn in GOA?  ")
+
+if hmm != GOA:
+    hmm = input("lookes like we fount antorher npc  ")
+
+
+elif hmm == GOA:
+    print("GOOD!")
+
+
+yes = ("yes")
+
+qwestion = input ("are you in grup 13?  ")
+
+
+
+
+if qwestion == yes:
+    qwestion = ("okey so we are in the same grup  ")
+
+
+elif qwestion != yes:
+    qwestion = input("oh okey  ")
+
+
+
+mentor = ("gabrieli")
+
+
+grup_mentor = input("who is your mentor?  ")
+
+if grup_mentor == mentor:
+    grup_mentor = input("ohh we are in thesame grup  ")
+
+
+elif grup_mentor != mentor:
+    mentor = input("wha you are ling  ")
