100f8cc84ba04a973d8a3972a11f7e2ea118953e
demetrebiga <demebiga@gmail.com>
2024-05-15 14:53:06 +0400

    second commit

---
+++ day30/lesson30.py
@@ -0,0 +1,10 @@
+fav_cars = ["toyota", "BMW", "jeep", "shevrolet camaro"]
+
+print (fav_cars [2] )
+
+if fav_cars != ("jeep"):
+    input ("enter better car than jeep:  ")
+
+right = ("mercedes")
+
+
