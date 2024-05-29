b22bab52272f2d25542fa77354fc21de90dae733
demetrebiga <demebiga@gmail.com>
2024-05-29 22:32:56 +0400

    second commit

---
+++ day34/lesson3.py
@@ -0,0 +1,6 @@
+abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+print (abc[6])
+print (abc[14])
+print (abc[0])
+
