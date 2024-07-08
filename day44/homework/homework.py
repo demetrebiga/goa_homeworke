4b5fdf1687667f6b67094cc4f2c8a1a681d8423b
demetrebiga <demebiga@gmail.com>
2024-07-08 21:25:31 +0400

    second commit

---
+++ day44/homework/homework.py
@@ -0,0 +1,13 @@
+#for i in range(0,21):
+#    print(i)
+
+#for i in range(11):
+#    print(i)
+
+#time = input ("what is the time right now?:  ")
+#am = ("1am,2am,3am,4am,5am,6am,7am,8am,9am,10am,11am,12am")
+#pm = ("1pm,2pm,3pm,4pm,5pm,6pm,7pm,8pm,9pm,10pm,11pm,12pm")
+#if time == am:
+#    print("good morning!")
+#elif time == pm:
+#    print("good afternoon!")
---
+++ day45/homewrok45/homework.py
@@ -0,0 +1,14 @@
+#hot = ("30+")
+
+#cold = ("29-")
+
+
+#input ("is temperture above 30 degrees outside, enter 30+ if temperture is over 30 degrees if not enter 29-?:  ")
+
+
+#if input == hot:
+#    print("It's hot outside!")
+
+
+#elif input == cold:
+#    print("It's not too hot.")
