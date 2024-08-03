370d6d8f8c5aefe093052759cf4f5b89ded2a047
demetrebiga <demebiga@gmail.com>
2024-08-03 18:03:23 +0400

    second commit

---
+++ day53/claswrok/caswrok.py
@@ -0,0 +1,11 @@
+def market_walk():
+    print("go down stairs")
+    print("go right")
+    print("go left")
+    print("go forward")
+    print("go left")
+    print("open dore")
+    print("you are in the store")
+
+
+market_walk()
