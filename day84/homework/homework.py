43b117430c540e359b14442a25da35c90262bf69
demetrebiga <demebiga@gmail.com>
2024-11-21 16:11:23 +0400

    second commit

---
+++ day84/homework/homework.py
@@ -0,0 +1,40 @@
+#1
+#You only need one - Beginner
+
+#def check(seq, elem):
+#    return elem in seq
+
+#2
+#Count Odd Numbers below n
+
+#def odd_count(n):
+#    return n // 2
+
+#3
+#Beginner - Reduce but Grow
+
+#def grow(arr):
+#    result = 1
+#    for num in arr:
+#        result *= num
+#    return result
+
+#4
+#My head is at the wrong end!
+
+#def fix_the_meerkat(arr):
+#    return arr[::-1]
+
+#5
+#Transportation on vacation
+
+#def rental_car_cost(d):
+#    cost_per_day = 40
+#    total_cost = d * cost_per_day
+#
+#    if d >= 7:
+#        total_cost -= 50
+#    elif d >= 3:
+#        total_cost -= 20
+#
+#    return total_cost
