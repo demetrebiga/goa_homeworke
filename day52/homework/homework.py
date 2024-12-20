a76d2a067e5dea028e7dafe127a704ce49aa6a9d
demetrebiga <demebiga@gmail.com>
2024-12-20 16:57:13 +0400

    second commit

---
+++ day52/homework/homework.py
@@ -0,0 +1,30 @@
+#1
+#Vowel Count
+
+#def get_count(sentence):
+#    vowels = "aeiou"
+#    vowels_count = 0
+#    for char in sentence:
+#        if char.lower() in vowels:
+#            vowels_count += 1
+#    return vowels_count
+
+
+#2
+#Square Every Digit
+
+#def square_digits(num):
+#    l = []
+#    
+#    for i in str(num):
+#        l.append(str(int(i)**2))
+#        
+#    return int(''.join(l))
+
+
+#3
+#Sum of two lowest positive integers
+
+#def sum_two_smallest_numbers(numbers):
+#    numbers.sort()
+#    return numbers[0] + numbers[1]
