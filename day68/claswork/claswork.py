27ddb6b942e590e63cd97cc5b269c73f42f9eb21
demetrebiga <demebiga@gmail.com>
2025-02-13 18:18:41 +0400

    second commit

---
+++ day68/claswork/claswork.py
@@ -0,0 +1,30 @@
+#1
+
+#Who is going to pay for the wall?
+
+#def who_is_paying(name):
+#    if len(name) > 2:
+#        return [name, name[0:2]]
+#    else:
+#        return [name]
+
+
+#2
+
+#Remove the time
+
+#def shorten_to_date(long_date):
+#    partition = long_date.partition(',')
+#    return partition [0]
+
+
+#3
+
+#Century From Year
+
+
+#def century(year):
+#    century = year // 100
+#    if year % 100 == 0:
+#        return century
+#    return century + 1
---
+++ day68/homework/homework1.py
@@ -0,0 +1,25 @@
+#1
+
+#Find Multiples of a Number
+
+#def find_multiples(integer, limit):
+#    return [integer for integer in range(integer, limit+1,integer)]
+
+#2
+
+#Sum of two lowest positive integers
+
+
+#def sum_two_smallest_numbers(numbers):
+#    numbers.sort()
+#    return numbers[0] + numbers[1]
+
+
+#3
+
+#Simple Fun #28: Html End Tag By Start Tag
+
+#def html_end_tag_by_start_tag(start_tag):
+#    tag_name = start_tag.split()[0][1:]
+#    tag_name = tag_name.rstrip(">")
+#    return f"</{tag_name}>"
