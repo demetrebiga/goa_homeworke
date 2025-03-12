5f36b54757ba78de7d7f65315a44a2f71b1da607
demetrebiga <demebiga@gmail.com>
2025-03-12 14:30:45 +0400

    second commit

---
+++ day74/homework/homewrok.py
@@ -0,0 +1,51 @@
+#1
+
+#Area or Perimeter
+
+# def area_or_perimeter(l , w):
+#     return l * w if l == w else 2 * (l + w)
+
+
+#2
+
+#Will you make it?
+
+
+# def zero_fuel(distance_to_pump, mpg, fuel_left):
+#     return distance_to_pump <= (mpg * fuel_left)
+
+
+
+#3
+
+#Twice as old
+
+
+# def twice_as_old(dad_years_old, son_years_old):
+#     age_at_birth = dad_years_old - son_years_old
+#     twice_as_old = age_at_birth * 2
+#     return abs(dad_years_old - twice_as_old)
+
+
+
+
+
+#4
+
+#Calculate average
+
+
+# def find_average(numbers):
+#     return sum(numbers) / len(numbers) if numbers else 0
+
+
+
+
+#5
+
+#Will there be enough space?
+
+
+
+# def enough(cap, on, wait):
+#     return max(0, on + wait - cap)
