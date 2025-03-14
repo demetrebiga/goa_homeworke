a3eb61435e371ba3d30a25ff09531ff3fc2feae2
demetrebiga <demebiga@gmail.com>
2025-03-14 12:57:14 +0400

    second commit

---
+++ day75/homework/homework.py
@@ -0,0 +1,75 @@
+#1
+
+#Do I get a bonus?
+
+# def bonus_time(salary, bonus):
+#     if bonus == True:
+#         return f"${salary * 10}"
+#     else:
+#         return f"${salary}"
+
+
+#2
+
+#Century From Year
+
+# def century(year):
+#     century = year // 100
+#     if year % 100 == 0:
+#         return century
+#     return century + 1
+
+
+
+#3
+
+#Beginner - Lost Without a Map
+
+# def maps(a):
+#     return [number * 2 for number in a]
+
+
+#4
+
+#Find Maximum and Minimum Values of a List
+
+# def minimum(arr):
+#     return min(arr)
+
+# def maximum(arr):
+#     return max(arr)
+
+
+#5
+
+#Find numbers which are divisible by given number
+
+
+# def divisible_by(numbers, divisor):
+#     result = []
+#     for num in numbers:
+#         if num % divisor == 0:
+#             result.append(num)
+#     return result
+
+
+
+#6
+
+#Invert values
+
+# def invert(lst):
+#     x = 1
+#     return [-x for x in lst]
+
+
+
+#7
+
+#Get Nth Even Number
+
+
+# def nth_even(n):
+#     n_minus_one = n - 1
+#     even_number = 2 * n_minus_one
+#     return even_number
